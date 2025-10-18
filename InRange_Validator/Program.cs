42173efaecf017ee7f76f651b9b47e706c55fdc2
unknown <wbc2504@gmail.com>
2025-10-17 22:43:49 -0500

    Se realiza el ejercicio 15

---
+++ InRange_Validator/Program.cs
@@ -0,0 +1,14 @@
+﻿/**Descripción: Pide un número al usuario y verifica si está en el rango de 10 a 20 (ambos incluidos).*/
+
+Console.WriteLine("Digita un numero: ");
+double numero = double.Parse(Console.ReadLine());
+
+if (numero < 20 && numero > 10)
+{
+    Console.WriteLine($"Numero {numero} dentro del rango");
+}
+else
+{
+    Console.WriteLine($"Numero {numero} fuera del rango");
+}
+
