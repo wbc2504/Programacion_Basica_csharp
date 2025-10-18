b4813cdbc21f48741e74cd0033f8c390cfad2385
unknown <wbc2504@gmail.com>
2025-10-17 19:40:51 -0500

    Se realiza el ejercicio numero 4

---
+++ Circle_Perimeter/Program.cs
@@ -0,0 +1,4 @@
+﻿/*Pide al usuario el radio de un círculo y calcula su perímetro. */
+Console.WriteLine("Digita el radio del circulo: ");
+double radio = double.Parse(Console.ReadLine());
+Console.WriteLine($"El perimetro del circulo es: {2*Math.PI*radio}");
--- Root_or_Square/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿/*Pide al usuario un número. Si es positivo, devuelve su raíz
-cuadrada, de lo contrario, devuelve su cuadrado.   */
-Console.WriteLine("Digita un numero: ");
-double num = double.Parse(Console.ReadLine());
-if (num > 0) Console.WriteLine($"La raiz cuadrada del numero es: {Math.Sqrt(num)}");
-else Console.WriteLine($"El cuadrado del numero es: {num*num}");
