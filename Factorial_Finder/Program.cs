7b8483a0adc1fae6984c52e28001683803c1e9b1
unknown <wbc2504@gmail.com>
2025-10-17 22:25:20 -0500

    Se realiza el ejercicio 14

---
+++ Factorial_Finder/Program.cs
@@ -0,0 +1,11 @@
+﻿/*  Pide un número al usuario y devuelve el factorial de ese número. 
+ */
+Console.WriteLine("Digita un numero entero: ");
+int numero = int.Parse(Console.ReadLine() );
+int factorial = numero;
+for (int i = (numero -1); i > 0 ; i--)
+{
+   factorial = factorial*i;
+}
+
+Console.WriteLine($"El factorial del numero {numero} es: {factorial}");
