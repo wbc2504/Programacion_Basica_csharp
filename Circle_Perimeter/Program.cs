5771f06b0a1ba48d4cf0f72b16ecf0c942398320
unknown <wbc2504@gmail.com>
2025-10-17 19:56:04 -0500

    Se realiza el ejercicio numero 5

--- Circle_Perimeter/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿/*Pide al usuario el radio de un círculo y calcula su perímetro. */
-Console.WriteLine("Digita el radio del circulo: ");
-double radio = double.Parse(Console.ReadLine());
-Console.WriteLine($"El perimetro del circulo es: {2*Math.PI*radio}");
---
+++ Midweek_Day/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Solicita al usuario un número entre 1 y 7 y muestra el día de la
+semana correspondiente, pero solo considerando los días laborables. */
+
+
+Boolean validar = true;
+int num = 0;
+
+while (validar == true){
+Console.WriteLine("Digita un numero entre 1 y 7: ");
+num = int.Parse(Console.ReadLine());
+if (num > 7 || num < 1) 
+{
+    Console.WriteLine("El numero no esta dentro del rango solicitado");
+}
+else 
+{
+    validar =false;
+}
+}
+
+string dia = num switch
+            {
+                1 => "Lunes",
+                2 => "Martes",
+                3 => "Miércoles",
+                4 => "Jueves",
+                5 => "Viernes",
+                _ => "Número inválido. Solo se consideran días laborables (1-5)."
+            };
+Console.WriteLine(dia);
