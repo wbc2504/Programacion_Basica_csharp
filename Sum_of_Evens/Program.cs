315794131b79936e448d169da6c654d7958316c3
unknown <wbc2504@gmail.com>
2025-10-17 20:47:51 -0500

    Se realiza el ejercicio 8

---
+++ Sum_of_Evens/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Calcula y muestra la suma de los números pares entre 1 y 50  */
+
+Console.WriteLine("Suma de los numeros del 1 al 50");
+int cuenta = 0;
+for (int i = 2; i <= 50; i += 2)
+{
+     cuenta = cuenta + i;
+}
+
+Console.WriteLine($"La suma de los numeros del 1 al 50 es: {cuenta}");
+
+
+
+
+
