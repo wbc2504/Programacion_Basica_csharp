eba67fee4cd15b360e516d66d7edcf43a61325bc
unknown <wbc2504@gmail.com>
2025-10-17 20:36:38 -0500

    Se realiza el ejercicio 7

--- Tax_Calculator/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿/* Solicita al usuario su salario anual y, si este excede los 12000,
-muestra el impuesto a pagar que es el 15% del excedente.  */
-
-Console.WriteLine("Digita tu salario anual");
-double salario = double.Parse(Console.ReadLine());
-const double tope = 12000;
-if (salario > 12000) Console.WriteLine($"El impuesto a pagar es: {(salario - tope)*0.15}");
-else Console.WriteLine("No aplica para pagar impuestos");
