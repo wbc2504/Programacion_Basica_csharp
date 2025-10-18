e9bf67389f5e8aea04b4148d0e54839120e84c56
unknown <wbc2504@gmail.com>
2025-10-17 19:20:47 -0500

    Se realiza el ejercicio numero 2

---
+++ Double_or_Triple/Program.cs
@@ -0,0 +1,8 @@
+﻿﻿/*Solicita al usuario dos números. Si el primero es mayor, devuelva
+su doble, de lo contrario devuelva el triple del segundo.  */
+Console.WriteLine("Digita el primer numero: ");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digita el segundo numero: ");
+double num2 = double.Parse(Console.ReadLine());
+if (num1 > num2) Console.WriteLine($"El doble del primer numero es: {num1 * 2}");
+else Console.WriteLine($"El triple del segundo numero es: {num2*3}");
